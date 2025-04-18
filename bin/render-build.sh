b8d00f4b7e457534d27bae8a09481e40b387f704
Vitor Queiroz <vitorqueiroz@MacBook-Pro-de-Vitor.local>
2025-04-18 19:49:16 -0300

    random assets error 2

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,6 @@
 set -o errexit
 
 bundle install
-RAILS_ENV=production bundle exec rake assets:precompile
-bundle exec rails assets:clean
 bundle exec rails db:migrate
 
 # If you're using a Free instance type, you need to
