61ac105072686bb546768451452adec23e1b1f84
Vitor Queiroz <vitorqueiroz@MacBook-Pro-de-Vitor.local>
2025-06-11 18:20:11 -0300

    removing crap

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
   get "pages/counter"
-  # Serve static files from app/public
-  get "/app/public/*path" => proc { |env|
-    file_path = Rails.root.join('app/public', env['action_dispatch.original_path'].sub('/app/public/', ''))
-    if File.exist?(file_path)
-      [200, { 'Content-Type' => Rack::Mime.mime_type(File.extname(file_path)) }, [File.read(file_path)]]
-    else
-      [404, { 'Content-Type' => 'text/plain' }, ['Not Found']]
-    end
-  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
